c5a38ba3993900b271a77a838ebc7a78058b22df
James Ruan <ruanbeihong@gmail.com>
2017-08-14 19:26:23 +0800

    use webpack

---
+++ make.bat
@@ -0,0 +1,3 @@
+md dist
+copy src/CopyRight.js src/Init.js src/Helper.js src/Polyfill.js src/HarmonyMaps.js src/TimeoutError.js src/setImmediate.js src/Future.js src/BytesIO.js src/Tags.js src/ClassManager.js src/Writer.js src/Reader.js src/Formatter.js src/ResultMode.js src/Client.js src/CookieManager.js src/HttpClient.js src/WebSocketClient.js src/ChromeTcpSocket.js src/APICloudTcpSocket.js src/TcpClient.js src/JSONRPCClientFilter.js src/Loader.js utils/regenerator-runtime.js dist/hprose-html5.src.js dist/hprose-html5.src.js
+webpack
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: './dist/hprose-html5.src.js',
+    output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "hprose-html5.js"
+	},
+	module: {
+		rules: [
+			{ test: /\.coffee$/, use: "coffee-loader" }
+		]
+	},
+	devtool: 'source-map',
+	plugins: [
+		new UglifyJSPlugin({
+			parallel: true,
+			sourceMap: true
+		})
+	]
+};
